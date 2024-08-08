4ba603b678e230019164f2aaf3600520dd8e15d7
W00LaLa <vv0o.1alah@gmail.com>
2024-08-08 11:38:56 +0100

    multiple catch-exception propagation

---
+++ cognizantrever/src/exceptions/TryCatchExample1.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class TryCatchExample1 {
+
+    public static void main(String[] args) {
+        try {
+            int data=50/0;
+        } catch (Exception e) {
+        }
+
+        System.out.println("rest of the code");  
+    } 
+}
